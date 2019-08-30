d45581f13ed24826575f72ede9732a8363db3943
Felipe Marques <felipedmarques22@gmail.com>
2019-08-29 21:15:35 -0300

    add game

---
+++ Assets/Script/Barreira.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barreira : MonoBehaviour
+{
+ 
+    public Vector2 velocity = new Vector2(-4, 0);
+    public float range = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+        transform.position =
+            new Vector3(transform.position.x,
+            transform.position.y - range * Random.value,
+            transform.position.z);
+ 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Gerador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gerador : MonoBehaviour
+{
+    public GameObject zezinho;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("createobstacle", 1f, 1.5f);
+        
+    }
+
+    // Update is called once per frame
+    void createobstacle()
+    {
+        Instantiate(zezinho);
+    }
+}
